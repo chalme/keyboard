e5b237f59c4ec29eb100eb5974c589be1c0b80ae
Chalme Cheng Peng <pc.chalme@gmail.com>
2024-03-17 01:57:06 +0800

    Update hyper-apps-defaults.lua

--- hammerspoon/hyper-apps-defaults.lua
+++ hammerspoon/hyper-apps-defaults.lua
@@ -4,13 +4,13 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 return {
-  { 'a', 'Music' },             -- "A" for "Apple Music"
+  { 'a', 'Spotify' },             -- "A" for "Apple Music"
   { 'b', 'Google Chrome' },     -- "B" for "Browser"
-  { 'c', 'Slack' },             -- "C for "Chat"
-  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Atom' },              -- "E" for "Editor"
+  { 'c', 'Telegram' },             -- "C for "Chat"
+  { 'd', 'Microsoft To Do' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Visual Studio Code' },              -- "E" for "Editor"
   { 'f', 'Finder' },            -- "F" for "Finder"
-  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
+  { 'g', 'Mail' },       -- "G" for "Gmail"
   { 's', 'Slack' },             -- "S" for "Slack"
   { 't', 'iTerm' },             -- "T" for "Terminal"
 }
