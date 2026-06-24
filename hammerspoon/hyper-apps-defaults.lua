a9553ac0932f3c810ec9f6ba7ad9ace9e27cddbb
chalme <pc.chalme@gmail.com>
2026-06-24 21:23:14 +0800

    Update default hyper app mappings

--- hammerspoon/hyper-apps-defaults.lua
+++ hammerspoon/hyper-apps-defaults.lua
@@ -4,16 +4,16 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 return {
-  { 'a', 'Spotify' },             -- "A" for "Apple Music"
-  { 'b', 'Google Chrome' },     -- "B" for "Browser"
-  { 'c', 'Telegram' },             -- "C for "Chat"
-  { 'd', 'Logseq' }, -- "D" for "Do!" ... or "Done!"
-  { 'q', 'Octarine' },
-  { 'e', 'Zed' },              -- "E" for "Editor"
-  { 'f', 'Finder' },            -- "F" for "Finder"
-  { 'g', 'Mail' },       -- "G" for "Gmail"
-  { 'r', 'ChatGPT'},       -- 'R' for 'ChatGPT'
-  { 's', 'Slack' },             -- "S" for "Slack"
-  { 't', 'iTerm' },             -- "T" for "Terminal"
-  { 'x', 'IntelliJ IDEA' }   
+    { 'a', 'Spotify' },     -- "A" for "Apple Music"
+    { 'b', 'Google Chrome' }, -- "B" for "Browser"
+    { 'c', 'Telegram' },    -- "C for "Chat"
+    { 'd', 'Logseq' },      -- "D" for "Do!" ... or "Done!"
+    { 'q', 'Octarine' },
+    { 'e', 'Zed' },         -- "E" for "Editor"
+    { 'f', 'Finder' },      -- "F" for "Finder"
+    { 'g', 'Mail' },        -- "G" for "Gmail"
+    { 'r', 'ChatGPT' },     -- 'R' for 'ChatGPT'
+    { 's', 'Slack' },       -- "S" for "Slack"
+    { 't', 'cmux' },        -- "T" for "Terminal"
+    { 'x', 'IntelliJ IDEA' }
 }
