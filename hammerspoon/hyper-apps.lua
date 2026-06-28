4f4b8b3ad90986fb9941f37d403b70389c3e3a4c
chalme <pc.chalme@gmail.com>
2026-06-28 18:42:26 +0800

    feat: add personal Hyper Mode app mappings (Codex on Hyper+d)

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,14 @@
+return {
+  { 'a', 'Spotify' },
+  { 'b', 'Google Chrome' },
+  { 'c', 'Telegram' },
+  { 'd', 'Codex' },
+  { 'q', 'Octarine' },
+  { 'e', 'Zed' },
+  { 'f', 'Finder' },
+  { 'g', 'Mail' },
+  { 'r', 'ChatGPT'},
+  { 's', 'Slack' },
+  { 't', 'cmux' },
+  { 'x', 'IntelliJ IDEA' },
+}
